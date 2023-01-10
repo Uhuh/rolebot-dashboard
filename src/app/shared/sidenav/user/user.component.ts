f32e1e025d4e03d3c459b70081c542df8d919ebf
Dylan Warren <dylanwarren19@gmail.com>
2023-01-10 08:25:01 -0600

    feat: user area, should replace login button and allow logout

---
+++ src/app/shared/sidenav/user/user.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { combineLatest } from 'rxjs';
+import { JwtService } from '../../services/jwtHandler.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+})
+export class UserComponent {
+  username?: string;
+  userId?: string;
+  avatarId?: string;
+  constructor(private readonly jwtHandler: JwtService) {
+    combineLatest([
+      this.jwtHandler.username$,
+      this.jwtHandler.avatar$,
+      this.jwtHandler.userId$,
+    ]).subscribe(([username, avatarId, userId]) => {
+      this.username = username;
+      this.userId = userId;
+      this.avatarId = avatarId;
+    });
+  }
+
+  get avatar() {
+    return `https://images.discordapp.net/avatars/${this.userId}/${this.avatarId}`;
+  }
+}
