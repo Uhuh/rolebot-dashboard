148564c0769f9bc335924ca1d8b0dc92a0343e80
Dylan Warren <dylanwarren19@gmail.com>
2023-01-10 08:25:30 -0600

    feat: basic app configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/servers/servers.module').then((m) => m.ServersModule),
+  },
+  {
+    path: 'dashboard/:guildId',
+    loadChildren: () =>
+      import('./modules/server/server.module').then((m) => m.ServerModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import Cookies from 'js-cookie';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInceptor } from './shared/interceptors/http-interceptor';
+import { SidenavModule } from './shared/sidenav/sidenav.module';
+import { COOKIES } from './shared/tokens/cookies.token';
+import { LOCAL_STORAGE } from './shared/tokens/localStorage.token';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SidenavModule, HttpClientModule],
+  providers: [
+    {
+      provide: COOKIES,
+      useValue: Cookies,
+    },
+    {
+      provide: LOCAL_STORAGE,
+      useValue: window.localStorage,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
