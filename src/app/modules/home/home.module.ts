f063c91e56facdf3ed0c49f71b963c15d432ac11
Dylan Warren <dylanwarren19@gmail.com>
2023-01-12 00:50:28 -0600

    update: some home page for basic tutorials later

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     RouterModule,
+    MatIconModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }]),
   ],
   exports: [HomeComponent],
