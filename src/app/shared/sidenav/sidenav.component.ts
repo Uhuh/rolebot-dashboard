877034b751487ce6725ffee9c865011c935546f2
Dylan Warren <dylanwarren19@gmail.com>
2023-01-19 23:26:42 -0600

    update: better sidenav layout to display current server

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { combineLatest, first } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, Subject } from 'rxjs';
 import { JwtService } from '../services/jwtHandler.service';
 import { IGuild } from '../types/interfaces';
 
@@ -9,32 +9,39 @@ import { IGuild } from '../types/interfaces';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnDestroy {
+  private readonly destroyed$ = new Subject<void>();
+  discordIconUrl = 'https://cdn.discordapp.com/icons';
+  loginUrl =
+    'https://discord.com/api/oauth2/authorize?client_id=741682757486510081&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fservers&response_type=code&scope=identify%20guilds';
+
   isFresh?: boolean = false;
   guilds: IGuild[] = [];
   selectedGuild?: IGuild;
-  discordIconUrl = 'https://cdn.discordapp.com/icons';
 
   constructor(
     private readonly jwtHandler: JwtService,
-    private readonly route: Router
+    private readonly route: ActivatedRoute
   ) {
     combineLatest([
       this.jwtHandler.isFresh$,
       this.jwtHandler.guilds$,
-      this.route.events,
-    ]).subscribe(([isFresh, guilds, event]) => {
+      this.route.queryParams,
+    ]).subscribe(([isFresh, guilds, params]) => {
       if (!guilds) return;
 
       this.isFresh = isFresh;
       this.guilds = guilds;
+      const guildId = params['guildId'];
 
-      if (event instanceof NavigationEnd) {
-        this.selectedGuild = this.guilds.find((g) =>
-          this.route.url.toString().includes(g.id)
-        );
+      if (guildId) {
+        this.selectedGuild = this.guilds.find((g) => g.id === guildId);
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
   }
 
   get guildIcon() {
@@ -43,5 +50,9 @@ export class SidenavComponent {
 
   setSelected(guild: IGuild) {
     this.selectedGuild = guild;
+  }
+
+  removeSelected() {
+    this.selectedGuild = undefined;
   }
 }
