a4df84f2b5c80cc1cf439a5d6b74dc8c80b0d453
Dylan Warren <dylanwarren19@gmail.com>
2023-01-19 23:28:02 -0600

    feat: info component for when no server is selected

---
+++ src/app/shared/sidenav/info/info.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { JwtService } from '../../services/jwtHandler.service';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss'],
+})
+export class InfoComponent implements OnDestroy {
+  private readonly destroyed$ = new Subject<void>();
+  isFresh?: boolean;
+
+  constructor(private readonly jwtService: JwtService) {
+    this.jwtService.isFresh$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((isFresh) => (this.isFresh = isFresh));
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+  }
+}
