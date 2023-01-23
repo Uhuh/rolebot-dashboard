80fc790331fcf9fce3ea15c7e5c393879c5b8c66
Dylan Warren <dylanwarren19@gmail.com>
2023-01-22 21:42:39 -0600

    update: a category create button

--- src/app/modules/server/category/category.module.ts
+++ src/app/modules/server/category/category.module.ts
@@ -7,6 +7,8 @@ import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { MatIconModule } from '@angular/material/icon';
 import { CardRowModule } from 'src/app/shared/card/card-row/card-row.module';
 import { CategoryFormModule } from './category-form/category-form.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [CategoryComponent],
@@ -17,6 +19,8 @@ import { CategoryFormModule } from './category-form/category-form.module';
     CardRowModule,
     CategoryFormModule,
     CommonModule,
+    MatButtonModule,
+    MatTooltipModule,
     RouterModule,
     RouterModule.forChild([
       {
