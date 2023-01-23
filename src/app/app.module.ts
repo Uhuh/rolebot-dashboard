9db034868d8aa9a017bc9406259d72de21945840
Dylan Warren <dylanwarren19@gmail.com>
2023-01-22 21:43:50 -0600

    update: material defaults

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,18 @@ import { SidenavModule } from './shared/sidenav/sidenav.module';
 import { COOKIES } from './shared/tokens/cookies.token';
 import { LOCAL_STORAGE } from './shared/tokens/localStorage.token';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SidenavModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SidenavModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   providers: [
     {
       provide: COOKIES,
@@ -27,6 +35,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInceptor,
       multi: true,
+    },
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3500 } },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
     },
   ],
   bootstrap: [AppComponent],
