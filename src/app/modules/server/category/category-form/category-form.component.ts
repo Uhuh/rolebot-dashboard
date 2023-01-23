90eeef17e325c7379d9196aab2d38de9662b7016
Dylan Warren <dylanwarren19@gmail.com>
2023-01-22 21:42:58 -0600

    update: form to allow users to update categories

--- src/app/modules/server/category/category-form/category-form.component.ts
+++ src/app/modules/server/category/category-form/category-form.component.ts
@@ -1,5 +1,21 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subject, takeUntil } from 'rxjs';
+import { ApiService } from 'src/app/shared/services/api.service';
 import { DisplayType, ICategory } from 'src/app/shared/types/interfaces';
 
 @Component({
@@ -7,35 +23,83 @@ import { DisplayType, ICategory } from 'src/app/shared/types/interfaces';
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.scss'],
 })
-export class CategoryFormComponent implements OnChanges {
+export class CategoryFormComponent implements OnChanges, OnDestroy {
+  private readonly destroyed$ = new Subject<void>();
+
   @Input() category!: ICategory;
+  @Output() categoryChange = new EventEmitter<ICategory>();
 
-  categoryGroup: FormGroup;
+  categoryForm: FormGroup;
 
-  constructor(private readonly fb: FormBuilder) {
-    this.categoryGroup = this.fb.group<Omit<ICategory, 'id' | 'guildId'>>({
-      name: '',
-      description: '',
-      displayOrder: DisplayType.alpha,
-      excludedRoleId: '',
-      mutuallyExclusive: false,
-      requiredRoleId: '',
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly apiService: ApiService,
+    private readonly snackBar: MatSnackBar
+  ) {
+    this.categoryForm = this.fb.group({
+      name: new FormControl('', [Validators.required, Validators.minLength(1)]),
+      description: new FormControl('', [Validators.maxLength(250)]),
+      displayOrder: new FormControl(`${DisplayType.alpha}`),
+      excludedRoleId: new FormControl(''),
+      mutuallyExclusive: new FormControl(''),
+      requiredRoleId: new FormControl(''),
     });
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges) {
     const { category } = changes;
+    console.log(category);
     if (category) {
       const value = category.currentValue as ICategory;
 
-      this.categoryGroup = new FormGroup({
-        name: new FormControl(value.name),
-        description: new FormControl(value.description),
-        displayOrder: new FormControl(value.displayOrder),
-        excludedRoleId: new FormControl(value.excludedRoleId),
-        mutuallyExclusive: new FormControl(value.mutuallyExclusive),
-        requiredRoleId: new FormControl(value.requiredRoleId),
+      this.categoryForm.patchValue({
+        name: value.name,
+        description: value.description,
+        displayOrder: `${value.displayOrder}`,
+        excludedRoleId: value.excludedRoleId,
+        mutuallyExclusive: value.mutuallyExclusive,
+        requiredRoleId: value.requiredRoleId,
       });
     }
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+  }
+
+  get isPristine() {
+    return this.categoryForm.pristine;
+  }
+
+  get isValid() {
+    return this.categoryForm.valid;
+  }
+
+  onSubmit() {
+    if (!this.isValid) {
+      return this.snackBar.open('Invalid form!', 'Dismiss', {
+        politeness: 'assertive',
+        panelClass: 'app-notification-error',
+      });
+    }
+
+    const displayOrder = this.categoryForm.value.displayOrder;
+    const updatedCategory = {
+      ...this.category,
+      ...this.categoryForm.value,
+      displayOrder: displayOrder
+        ? Number(displayOrder)
+        : this.category.displayOrder,
+    };
+
+    return this.apiService
+      .updateCategory(this.category.guildId, updatedCategory)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((category) => {
+        this.snackBar.open(`Successfully updated category!`, 'Ok', {
+          panelClass: 'app-notification-success',
+        });
+        this.categoryForm.markAsPristine();
+      });
   }
 }
--- src/app/modules/server/category/category-form/category-form.module.ts
+++ src/app/modules/server/category/category-form/category-form.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { CategoryFormComponent } from './category-form.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [CategoryFormComponent],
   imports: [
     CommonModule,
+    MatButtonModule,
     MatCheckboxModule,
+    MatSnackBarModule,
     MatInputModule,
     MatTooltipModule,
     MatIconModule,
