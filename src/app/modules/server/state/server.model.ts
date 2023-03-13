7438a633fbacc9b4dff875af9ed5799a2788de73
Dylan Warren <dylanwarren19@gmail.com>
2023-03-13 00:38:38 -0500

    feat: guild emojis and roles

--- src/app/modules/server/state/server.model.ts
+++ src/app/modules/server/state/server.model.ts
@@ -1,11 +1,15 @@
 import {
   ICategory,
   IGuildConfig,
+  IGuildEmoji,
+  IGuildRole,
   IReactRole,
 } from 'src/app/shared/types/interfaces';
 
 export interface GuildState {
   guildId: string;
+  guildRoles: IGuildRole[];
+  guildEmojis: IGuildEmoji[];
   config: IGuildConfig;
   categories: ICategory[];
   reactRoles: IReactRole[];
