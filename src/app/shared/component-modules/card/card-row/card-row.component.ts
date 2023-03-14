7a3149bf8a7f1a882c149144f9db1a316d04fa71
Dylan Warren <dylanwarren19@gmail.com>
2023-03-14 01:00:54 -0500

    chore: basic removal

--- src/app/shared/component-modules/card/card-row/card-row.component.ts
+++ src/app/shared/component-modules/card/card-row/card-row.component.ts
@@ -4,7 +4,6 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-card-row',
   templateUrl: './card-row.component.html',
   styleUrls: ['./card-row.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class CardRowComponent {
   @Input() expanded?: boolean;
