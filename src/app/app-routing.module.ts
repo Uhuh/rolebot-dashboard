63119ce5ce0ed231528fd00d248d0494169b551c
Dylan Warren <dylanwarren19@gmail.com>
2023-03-18 02:04:57 -0500

    update: remove unused home comp, redirect to servers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./modules/home/home.module').then((m) => m.HomeModule),
+    redirectTo: 'servers',
+    pathMatch: 'full',
   },
   {
     path: 'servers',
