3094856361bde84c7b2a15676336f8878d5bbc5c
Dylan Warren <dylanwarren19@gmail.com>
2023-03-18 01:45:35 -0500

    update: i should get env vars for this lol

--- src/app/shared/component-modules/sidenav/sidenav.component.ts
+++ src/app/shared/component-modules/sidenav/sidenav.component.ts
@@ -19,7 +19,7 @@ export class SidenavComponent implements OnDestroy {
   private readonly destroyed = new Subject<void>();
   discordIconUrl = 'https://cdn.discordapp.com/icons';
   loginUrl =
-    'https://discord.com/api/oauth2/authorize?client_id=741682757486510081&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fservers&response_type=code&scope=identify%20guilds';
+    'https://discord.com/api/oauth2/authorize?client_id=741682757486510081&redirect_uri=https%3A%2F%2Fdashboard.rolebot.gg%2Fservers&response_type=code&scope=identify%20guilds';
 
   links: SideNavLink[] = [
     {
